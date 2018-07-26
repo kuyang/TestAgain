cc2be70f84e74ef7c2f7126949671074720b74a8
tla111 <tlamarketing111@gmail.com>
2018-07-26 19:20:51 -0400

    made changes

---
+++ main.js
@@ -0,0 +1,13 @@
+let myName = prompt('What\'s your name?');
+let myAge = confirm('Are you older than 30');
+
+console.log(myName);
+console.log(myAge);
+
+if(myAge) {
+    console.log('Hello ' + myName + ' I\'m older than 30 too');
+} else {
+    console.log('Hello ' + myName + ' you\'re so young!');
+}
+
+alert('Hello World');
