60584ed514516c107bebdb3f685a99c735a7126e
tla111 <tlamarketing111@gmail.com>
2018-07-30 22:59:02 -0400

    made changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let myName = prompt('What\'s your name?');
 let mySchool = prompt('Which high school did you attend?');
 let mySubject = prompt('What was your favorite subject in high school?');
 let myState = confirm('Will you be working in Montgomery County?');
-let myMusic = confirm('Have you heard of Drake?')
+let myMusic = confirm('Have you heard of Drake?');
 
 alert('Hey ' + myName + '!' + ' I have heard of ' + mySchool + '. ' + mySubject + ' is a very interesting topic to study. ');
 
