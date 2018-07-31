dc273689b55ae9a36c2d14867ef98a22ca186055
tla111 <tlamarketing111@gmail.com>
2018-07-30 22:53:27 -0400

    made changes

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ let myState = confirm('Will you be working in Montgomery County?');
 alert('Hey ' + myName + '!' + ' I have heard of ' + mySchool + '. ' + mySubject + ' is a very interesting topic to study. ');
 
 console.log(myName);
-console.log(myAge);
-console.log(mySport);
-console.log(myHobby);
+console.log(mySchool);
+console.log(mySubject);
+console.log(myState);
 
 if(myState) {
     console.log('Hello ' + myName + ' I\'m older than 30 too');
