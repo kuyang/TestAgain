1813a5c526ec837a640b263be6d203689fe549a3
tla111 <tlamarketing111@gmail.com>
2018-07-30 22:55:11 -0400

    made changes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let myName = prompt('What\'s your name?');
 let mySchool = prompt('Which high school did you attend?');
 let mySubject = prompt('What was your favorite subject in high school?');
 let myState = confirm('Will you be working in Montgomery County?');
+let myMusic = confirm('Have you heard of Drake?')
 
 alert('Hey ' + myName + '!' + ' I have heard of ' + mySchool + '. ' + mySubject + ' is a very interesting topic to study. ');
 
@@ -9,6 +10,7 @@ console.log(myName);
 console.log(mySchool);
 console.log(mySubject);
 console.log(myState);
+console.log(myMusic);
 
 if(myState) {
     console.log('Hello ' + myName + ' I\'m older than 30 too');
