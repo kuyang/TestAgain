90c5400423cad36750519ff27facaa7c2f4023f6
tla111 <tlamarketing111@gmail.com>
2018-08-06 19:04:56 -0400

    made changes

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 let myName = prompt('What\'s your name?');
 let mySchool = prompt('Which high school did you attend?');
 let mySubject = prompt('What was your favorite subject in high school?');
+let myMovies = prompt('What is one of your top ten favorite movies of all time?');
 let myState = confirm('Will you be working in Montgomery County?');
 let myMusic = confirm('Have you heard of Drake?');
 
-alert('Hey ' + myName + '!' + ' I have heard of ' + mySchool + '. ' + mySubject + ' is a very interesting topic to study. ');
+alert('Hey ' + myName + '!' + ' I have heard of ' + mySchool + '. ' + mySubject + ' is a very interesting topic to study. ' 
+        + ' I always wanted to watch ' + myMovies + '. ');
 
 console.log(myName);
 console.log(mySchool);
 console.log(mySubject);
+console.log(myHobby);
 console.log(myState);
 console.log(myMusic);
 
